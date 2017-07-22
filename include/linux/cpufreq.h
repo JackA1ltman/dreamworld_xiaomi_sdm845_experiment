cd87fe1b42477c3b70835739cd49e25cef26d9e6
Dietmar Eggemann <dietmar.eggemann@arm.com>
2017-07-22 18:13:50 +0100

    cpufreq: Provide default frequency-invariance setter function
    Frequency-invariant accounting support based on the ratio of current
    frequency and maximum supported frequency is an optional feature an arch
    can implement.
    
    Since there are cpufreq drivers (e.g. cpufreq-dt) which can be build for
    different arch's a default implementation of the frequency-invariance
    setter function arch_set_freq_scale() is needed.
    
    This default implementation is an empty weak function which will be
    overwritten by a strong function in case the arch provides one.
    
    The setter function passes the cpumask of related (to the frequency
    change) cpus (online and offline cpus), the (new) current frequency and
    the maximum supported frequency.
    
    Cc: Rafael J. Wysocki <rjw@rjwysocki.net>
    Cc: Viresh Kumar <viresh.kumar@linaro.org>
    Signed-off-by: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Change-Id: I912d5815ee29e1171c498e638d1a089c5a598add
    Signed-off-by: Chris Redpath <chris.redpath@arm.com>
    Git-commit: 5afe8346225192213acf8a4cdcb98dc51b09929e
    Git-repo: https://android.googlesource.com/kernel/common/
    Signed-off-by: Satya Durga Srinivasu Prabhala <satyap@codeaurora.org>
    [mydongistiny: Fixed called function for crosshatch]
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: CloudedQuartz <ravenklawasd@gmail.com>
    Signed-off-by: Sorayukii <Sorayukii69@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -933,6 +933,10 @@ int cpufreq_generic_init(struct cpufreq_policy *policy,
 		struct cpufreq_frequency_table *table,
 		unsigned int transition_latency);
 
+extern unsigned int cpuinfo_max_freq_cached;
+void scale_freq_capacity(const cpumask_t *cpus, unsigned long cur_freq,
+			 unsigned long max_freq);
+
 struct sched_domain;
 unsigned long cpufreq_scale_freq_capacity(struct sched_domain *sd, int cpu);
 unsigned long cpufreq_scale_max_freq_capacity(struct sched_domain *sd, int cpu);
